0b437ca7ee32afa31e4c0656d3880b531683a53c
Amayuru1999 <amarasingheau@gmail.com>
2025-10-28 22:33:04 +0530

    chore: refactor the codebase

--- doctracer/cli/__init__.py
+++ doctracer/cli/__init__.py
@@ -1,6 +1,5 @@
 import click
 from doctracer.cli.extract import extract
-from doctracer.cli.track import track
 
 @click.group()
 def cli():
@@ -9,7 +8,6 @@ def cli():
 
 # Register subcommands
 cli.add_command(extract, name='extract')
-cli.add_command(track,   name='track')
 
 if __name__ == "__main__":
     cli()
